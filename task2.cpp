12c64774818fd307b3d38b8f4a70cc5feab595b5
donymie <sashadonchak@gmail.com>
2017-10-29 22:41:24 +0300

    t

---
+++ task2.cpp
@@ -0,0 +1,17 @@
+// task2.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+	int i;
+	for (i = 26; i >= 15; i--)
+	{
+		cout << i << " ";
+	}
+	cin.get();
+}
+
+
